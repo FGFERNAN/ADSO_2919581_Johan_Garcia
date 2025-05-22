d7d8fcab2dc087d3617464c6b679402e81b33908
FGFERNAN <fgfernan2508@gmail.com>
2025-05-22 17:23:09 -0500

    .
    .

--- actividades_en_clase/automatización/AutomatizacionDemoQA/AutomatizacionDemoQA/src/test/java/co/com/AutomatizacionDemoQA/runners/RegistroDemoQARunner.java
+++ actividades_en_clase/automatización/AutomatizacionDemoQA/AutomatizacionDemoQA/src/test/java/co/com/AutomatizacionDemoQA/runners/RegistroDemoQARunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         features = "src/test/resources/features/registro_demoQA.feature",
-        glue = {"co.com.AutomatizacionDemoQA.stepsdefinitions", "co.com.AutomatizacionDemoQA.utils.hooks"},
+        glue = {"co.com.AutomatizacionDemoQA.stepsDefinitions", "co.com.AutomatizacionDemoQA.utils.hooks"},
         snippets = SnippetType.CAMELCASE)
 
 public class RegistroDemoQARunner {
